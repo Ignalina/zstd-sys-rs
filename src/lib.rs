d2be67f98ef21dba89893b23bfb87ee44076047f
Rickard Lundin <rickard@ignalina.dk>
2025-07-24 17:27:11 +0200

    shaping

--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
-
-// Laddar bindgen-bindningarna från OUT_DIR
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
--- wrapper.h
+++
@@ -1,5 +0,0 @@
-#define ZSTD_MULTITHREAD 1
-#define ZSTD_STATIC_LINKING_ONLY 1
-
-#include "zstd.h"
-#include "zstd_errors.h"
